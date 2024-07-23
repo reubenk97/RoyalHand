d89d43e305ac1a27cdd1ef329fad190bd7adc802
reubenk97 <reubenk97@gmail.com>
2024-07-23 11:06:06 -0500

    add base gameboard updates

--- client/src/views/GameBoard.jsx
+++ client/src/views/GameBoard.jsx
@@ -11,19 +11,28 @@ const GameBoard = (props) => {
 
     return (
         <div className="game-board">
-            <div className="player-hand">
-                <Button type={'leave'} />
-                <div>VC</div>
-                <div>Player 1 Hand</div>
-                {/* <div>Player 1 Section</div> */}
+            <div className="game-board-top">
+                <div className="game-board-header">
+                    <Button type={'leave'} />
+                    <h2>VC</h2>
+                    <p className="tooltip">i</p>
+                </div>
+                <div className="player-hand">
+                    <div>Player 1 Avatar</div>
+                    <div>Player 1 Name</div>
+                    <div>Player 1 Hand</div>
+                    {/* <div>Player 1 Section</div> */}
+                </div>
             </div>
-            <div className="game-middle">
+            <div className="game-board-middle">
                 {/* <div>Deck</div> */}
                 <div className="card-pile">Play Pile</div>
                 <div className="card-pile">Discard Pile</div>
             </div>
-            <div>
+            <div className="player-hand">
                 {/* <div>Host Section</div> */}
+                <div>Host Avatar</div>
+                <div>Host Name</div>
                 <div>Host Hand</div>
             </div>
             {/* <div>Player 2 Hand</div>
